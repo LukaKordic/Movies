bec083b830690339ba71c07cf94a3e268bb5c65a
Luka <luka.kordic@cobeisfresh.com>
2019-11-22 23:38:01 +0100

    Create new file to store api keys and store tmdb key inside of it
    Add the file to .gitignore

--- app/src/main/java/com/example/lkord/movies/data/network/MovieAPI.kt
+++ app/src/main/java/com/example/lkord/movies/data/network/MovieAPI.kt
@@ -6,13 +6,13 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val API_KEY: String = "jfkla;jfkal;fjk;da"
+private const val API_KEY: String = "b02312b8e3c99d1bc9b7a02f2d75b205"
 
 interface MovieAPI {
-  
+
   @GET("movie/now_playing")
   fun getNowPlayingMovies(@Query("api_key") apiKey: String = API_KEY): Single<Response<MovieResponse>>
-  
+
   @GET("movie/popular")
   fun getPopularMovies(@Query("api_key") apiKey: String = API_KEY): Single<Response<MovieResponse>>
 }
