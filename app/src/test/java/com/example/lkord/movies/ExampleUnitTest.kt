b7c350431763e8dea5003c06dd325a918d6a7ead
Luka <luka.kordic@cobeisfresh.com>
2020-04-08 12:18:59 +0200

    Create a failing unit test to check if yaml script is working

--- app/src/test/java/com/example/lkord/movies/ExampleUnitTest.kt
+++ app/src/test/java/com/example/lkord/movies/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.lkord.movies
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +9,13 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+  @Test
+  fun addition_isCorrect() {
+    assertEquals(4, 2 + 2)
+  }
+
+  @Test
+  fun `test if addition is wrong`() {
+    assertEquals(5, 2 + 2)
+  }
 }
