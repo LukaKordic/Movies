6c26e30116eab90f0a2a95c89ab84ca053b1ab93
Luka <luka.kordic@cobeisfresh.com>
2020-04-09 17:50:52 +0200

    Modify path of decrypted file in the script

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE_DISTRIBUTION" \
---output $HOME/secrets/movies-d8bf0-96de52f83acf.json movies-d8bf0-96de52f83acf.json.gpg
+--output $HOME/movies-d8bf0-96de52f83acf.json movies-d8bf0-96de52f83acf.json.gpg
