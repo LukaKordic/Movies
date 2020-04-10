31f868f1af8354b2f24997babc029844100dcf05
Luka <luka.kordic@cobeisfresh.com>
2020-04-10 14:53:59 +0200

    Remove firebase distribution gradle plugin

--- .github/scripts/decrypt_secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE_DISTRIBUTION" \
---output $HOME/movies-d8bf0-96de52f83acf.json movies-d8bf0-96de52f83acf.json.gpg
