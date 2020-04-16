15452f8cb0c14ea86bdc40e64c4ab49574f23f98
Luka <luka.kordic@cobeisfresh.com>
2020-04-16 16:51:46 +0200

    Swap api key for auth token and auth header

--- data/src/main/java/com/example/data/networking/MovieAPI.kt
+++ data/src/main/java/com/example/data/networking/MovieAPI.kt
@@ -9,8 +9,8 @@ import retrofit2.http.Query
 interface MovieAPI {
 
   @GET("movie/now_playing")
-  suspend fun getNowPlayingMovies(@Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY): Response<MovieResponseWrapper>
+  suspend fun getNowPlayingMovies(@Query("api_key") apiKey: String = BuildConfig.TMDB_API_TOKEN): Response<MovieResponseWrapper>
 
   @GET("movie/popular")
-  suspend fun getPopularMovies(@Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY): Response<MovieResponseWrapper>
+  suspend fun getPopularMovies(@Query("api_key") apiKey: String = BuildConfig.TMDB_API_TOKEN): Response<MovieResponseWrapper>
 }
