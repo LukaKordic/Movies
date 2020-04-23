f91cefc99c8940602d4062be80cbac0e7b041062
Luka <luka.kordic@cobeisfresh.com>
2020-04-23 09:20:54 +0200

    Create `subscribe` extension function for observing LiveData objects
    Add `liveData` coroutine builder in `NowPlayingViewModel`

--- app/src/main/java/com/example/lkord/movies/ui/nowPlaying/view/NowPlayingFragment.kt
+++ app/src/main/java/com/example/lkord/movies/ui/nowPlaying/view/NowPlayingFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.domain.model.Movie
 import com.example.lkord.movies.App
@@ -15,6 +14,7 @@ import com.example.lkord.movies.ui.moviedetails.startMovieDetailsActivity
 import com.example.lkord.movies.ui.nowPlaying.adapters.MovieAdapter
 import com.example.lkord.movies.util.extensions.getViewModel
 import com.example.lkord.movies.util.extensions.isVisible
+import com.example.lkord.movies.util.extensions.subscribe
 import com.example.lkord.movies.viewModels.NowPlayingViewModel
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_now_playing.*
@@ -40,8 +40,7 @@ class NowPlayingFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     initRecyclerView()
-    viewModel.fetchNowPlayingMovies()
-    viewModel.getNowPlayingViewState().observe(viewLifecycleOwner, Observer { it?.run(::onViewStateChanged) })
+    viewModel.nowPlayingViewState.subscribe(viewLifecycleOwner, this::onViewStateChanged)
   }
 
   private fun initRecyclerView() {
--- app/src/main/java/com/example/lkord/movies/viewModels/NowPlayingViewModel.kt
+++ app/src/main/java/com/example/lkord/movies/viewModels/NowPlayingViewModel.kt
@@ -1,18 +1,15 @@
 package com.example.lkord.movies.viewModels
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.liveData
 import com.example.domain.common.onFailure
 import com.example.domain.common.onSuccess
 import com.example.domain.interaction.GetNowPlayingMoviesUseCase
 import com.example.lkord.movies.ui.nowPlaying.view.Data
 import com.example.lkord.movies.ui.nowPlaying.view.Error
-import com.example.lkord.movies.ui.nowPlaying.view.Loading
 import com.example.lkord.movies.ui.nowPlaying.view.MovieListViewState
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class NowPlayingViewModel @Inject constructor(
@@ -20,19 +17,11 @@ class NowPlayingViewModel @Inject constructor(
 
   private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
     println(throwable.printStackTrace())
-    nowPlayingViewState.postValue(Error(throwable))
   }
 
-  private val nowPlayingViewState = MutableLiveData<MovieListViewState>()
-
-  fun getNowPlayingViewState() = nowPlayingViewState
-
-  fun fetchNowPlayingMovies() {
-    nowPlayingViewState.postValue(Loading)
-    viewModelScope.launch(coroutineExceptionHandler + Dispatchers.IO) {// TODO: 20/04/2020 move this IO switch further down the call chain (repository)
-      getNowPlayingMoviesUseCase()
-          .onSuccess { nowPlayingViewState.postValue(Data(it)) }
-          .onFailure { nowPlayingViewState.postValue(Error(it)) }
-    }
+  val nowPlayingViewState = liveData<MovieListViewState>(coroutineExceptionHandler + Dispatchers.IO) {
+    getNowPlayingMoviesUseCase()
+        .onSuccess { emit(Data(it)) }
+        .onFailure { emit(Error(it)) }
   }
 }
